d7612444fa4116018d160452e6cf64f7d5d2bfdc
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-09 17:21:45 +0800

    Add rehype extension for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,30 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 import remarkDirective from "remark-directive";
 import remarkDirectiveToHtml from "./src/plugins/remark-to-directive-to-html";
-// import remarkHint from "remark-hint";
+import { transformerCopyButton } from "@rehype-pretty/transformers/copy-button";
+import remarkRehype from "remark-rehype";
 
 export default defineConfig({
   markdown: {
-    // syntaxHighlight: "prism",
+    syntaxHighlight: false,
     remarkPlugins: [remarkDirective, remarkDirectiveToHtml],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          transformers: [
+            transformerCopyButton({
+              visibility: "hover",
+              feedbackDuration: 3_000,
+            }),
+          ],
+        },
+      ],
+    ],
     // gfm: true,
   },
   integrations: [react(), mdx()],
