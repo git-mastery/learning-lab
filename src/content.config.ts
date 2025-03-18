9d7a3132c397e6240195a6b040f5130622b4c4d6
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-18 10:54:26 +0800

    Add tours routing and rendering

--- src/content.config.ts
+++ src/content.config.ts
@@ -25,4 +25,27 @@ const lessons = defineCollection({
   }),
 });
 
-export const collections = { lessons };
+const tours = defineCollection({
+  loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./tours" }),
+  schema: z.object({
+    title: z.string(),
+    draft: z.boolean().optional().default(false),
+    description: z.string().optional(),
+    categories: z.array(z.string()).optional().default([]),
+    order: z.number().optional(),
+    next: z
+      .object({
+        path: z.string(),
+        name: z.string(),
+      })
+      .optional(),
+    prev: z
+      .object({
+        path: z.string(),
+        name: z.string(),
+      })
+      .optional(),
+  }),
+});
+
+export const collections = { lessons, tours };
