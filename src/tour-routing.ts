07ea124241389e2bd8d14edb33829ef9bce3ac4a
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-31 10:47:48 +0800

    Adopt linear table layout for tours

--- src/tour-routing.ts
+++ src/tour-routing.ts
@@ -7,16 +7,16 @@ export interface Tour {
 
 export const tours: Tour[] = [
   {
+    path: "my-folder-my-repo",
+    title: "My Folder, My Repo",
+    description: "Starting from an empty folder, how do we start using Git?",
+    goal: "Understand the fundamental of initializing a Git repository, and saving snapshots of the repository.",
+  },
+  {
     path: "introduction",
     title: "Introduction",
     description:
       "Introduction to the history and fundamental concepts of the Git version control system.",
     goal: "Understand and articulate the origins of Git.",
-  },
-  {
-    path: "my-folder-my-repo",
-    title: "My Folder, My Repo",
-    description: "Starting from an empty folder, how do we start using Git?",
-    goal: "Understand the fundamental of initializing a Git repository, and saving snapshots of the repository.",
   },
 ];
